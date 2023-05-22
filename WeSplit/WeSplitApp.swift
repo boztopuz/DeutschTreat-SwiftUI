39e385b2a2187ebacb762fb4e596121a01d8472f
boztopuz <boztopuz@gmail.com>
2023-05-22 21:50:20 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Burak Öztopuz on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
